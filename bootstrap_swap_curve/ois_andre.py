7d59b09ff8f8672252c8d1a85f49076cd08c9a22
andreignatius <andre.callme@gmail.com>
2024-03-10 23:09:20 +0800

    all pv and df for all tenors

--- bootstrap_swap_curve/ois_andre.py
+++ bootstrap_swap_curve/ois_andre.py
@@ -1,4 +1,5 @@
 import math
+import json
 
 d1 = 1 / (1 + 0.003)
 # r1 = ( ( 1 / ( d1 ** (1/360) ) ) - 1 ) * 360
@@ -95,17 +96,43 @@ discount_factors = [
     1/1.003,
 ]
 
-rate = [
-    0.00300,
-    0.00325,
-    0.00335,
-    0.00350,
-    0.00360,
+# rate = [
+#     0.00300,
+#     0.00325,
+#     0.00335,
+#     0.00350,
+#     0.00360,
+# ]
+
+rates_table = {
+    # '1Y' : { 'rate' : 0.00300, 'diff': 1 },
+    '2Y' : { 'rate' : 0.00325, 'diff': 1 },
+    '3Y' : { 'rate' : 0.00335, 'diff': 1 },
+    '4Y' : { 'rate' : 0.00350, 'diff': 1 },
+    '5Y' : { 'rate' : 0.00360, 'diff': 1 },
+    '7Y' : { 'rate' : 0.00400, 'diff': 2 },
+    '10Y': { 'rate' : 0.00450, 'diff': 3 },
+    '15Y': { 'rate' : 0.00500, 'diff': 5 },
+    '20Y': { 'rate' : 0.00525, 'diff': 5 },
+    '30Y': { 'rate' : 0.00550, 'diff': 10 },
+}
+
+benchmark_tenors_involving_interpolation = [
+    '5Y', '7Y', '10Y', '15Y', '20Y', '30Y',
 ]
+
+# for tenor in rates_table:
+# 	print("rate: ", rates_table[tenor]['rate'], "diff: ", rates_table[tenor]['diff'])
 
 l1 = d1 * 0.003
 pv_prev = l1
-for i in range(1, 5):
+
+coefficients = []
+numerators = []
+
+results = {}
+# for i in range(1, 5):
+for tenor in rates_table:
 	# print("discount_factors: ", discount_factors)
 	# print("rate["+str(i)+"]: ", rate[i])
 	# discount_factor_n_1 = ( pv_prev - rate[i] * sum(discount_factors) + discount_factors[-1] ) / ( ( rate[i] + 1 ) * discount_factors[-1] )
@@ -113,7 +140,51 @@ for i in range(1, 5):
 	# print("D("+str(i)+","+str(i+1)+"): ", discount_factor_n_1)
 	# print("D(0,"+str(i+1)+"): ", discount_factor)
 
-	discount_factor = ( 1 - rate[i] * sum(discount_factors) ) / ( rate[i] + 1 )
+	print("check rate: ", rates_table[tenor]['rate'])
+
+	if rates_table[tenor]['diff'] == 1:
+		discount_factor = ( 1 - rates_table[tenor]['rate'] * sum(discount_factors) ) / ( rates_table[tenor]['rate'] + 1 )
+	else:
+		# discount_factor_tmp = sum(discount_factors)
+		discount_factor_tmp = 0
+		for i in range(5):
+			discount_factor_tmp += discount_factors[i]
+
+		factor = sum([ i for i in range(rates_table[tenor]['diff']) ]) / rates_table[tenor]['diff']
+		factor1 = factor + 1
+		print("check factor: ", factor , " factor1 : ", factor1)
+		# factor * discount_factors[-1]
+		if len(coefficients) == 0:
+			coefficients.extend([factor, factor1])
+		else:
+			coefficients[-1] = coefficients[-1] + factor
+			coefficients.append(factor1)
+		print("coefficients: ", coefficients)
+		for i in range(len(coefficients)):
+			print("tenor: ", benchmark_tenors_involving_interpolation[i], " coefficient: ", coefficients[i])
+
+		numerator = coefficients[-2] * discount_factors[-1]
+		numerators.append(numerator)
+		print("numerators: ", numerators)
+
+		# add_interpolation_discount_factors = []
+		# add_factors = []
+		# for i in range(len(numerators)):
+		# 	num = numerators[i]
+		# 	num = num * coefficients[i]
+		# 	add_factors.append(num)
+		# print("check add_factors: ", add_factors)
+
+		discount_factor = ( 1 - rates_table[tenor]['rate'] * ( discount_factor_tmp + sum(numerators) ) ) / ( factor1 * rates_table[tenor]['rate'] + 1 )
+		print("discount_factor[" , tenor, "]: ", discount_factor)
+		
+		# discount_factors.append( discount_factor )
+		# print("check discount_factors111: ", discount_factors)
+		# continue
+
+
+
+	# discount_factor = ( 1 - rate[i] * sum(discount_factors) ) / ( rate[i] + 1 )
 
 	discount_factors.append(discount_factor)
 	# print(i, discount_factors[i])
@@ -136,9 +207,10 @@ for i in range(1, 5):
 	tmp = ( 1 / discount_factor ) * ( pv - pv_prev )
 	f_val = 360 * (math.pow((tmp+1), 1/360) - 1)
 	# print("dis_fac:", discount_factors[i], "\tf"+str(i+1), f_val, "\tpv"+str(i+1), pv)
-	print("dis_fac:", discount_factor, "\tf"+str(i+1), f_val, "\tpv"+str(i+1), pv)
+	print("dis_fac:", discount_factor, "\tf"+str(tenor.replace('Y','')), f_val, "\tpv"+str(tenor.replace('Y','')), pv)
 	pv_prev = pv
+
+	results[tenor] = {'dis_fac' : discount_factor, 'pv' : pv}
 	
-
-
+print(json.dumps(results, indent=4))
 
